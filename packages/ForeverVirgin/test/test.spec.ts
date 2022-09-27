e85c276330aab4d5be13fb166befba3ad867ab7f
Vladislav Ch <hvostulitki@gmail.com>
2022-09-27 13:32:52 +0800

    1324

--- packages/ForeverVirgin/test/test.spec.ts
+++ packages/ForeverVirgin/test/test.spec.ts
@@ -3,9 +3,9 @@ import { expect } from "chai";
 
 describe("Example testing of module `module`", () => {
   it("test 1", () => {
-    expect(countDays(new Date("2022-09-27"))).to.be.equal("До нового года осталось 13 недель, 4 дней, 16 часов, 0 минут, 0 секунд, 0 миллисекунд.");
+    expect(countDays(new Date("2022-09-27"))).to.be.equal("До нового года осталось 13 недель, 5 дней, 0 часов, 0 минут, 0 секунд, 0 миллисекунд.");
   });
   it("test 2", () => {
-    expect(countDays(new Date("2022-12-30"))).to.be.equal("До нового года осталось 0 недель, 1 дней, 16 часов, 0 минут, 0 секунд, 0 миллисекунд.");
+    expect(countDays(new Date("2022-12-30"))).to.be.equal("До нового года осталось 0 недель, 2 дней, 0 часов, 0 минут, 0 секунд, 0 миллисекунд.");
   });
 });
