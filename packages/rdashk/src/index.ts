a72be440e0526ad54592063fbe129d48db94b4a8
rdashk <rdashak8@gmail.com>
2022-09-27 13:43:16 +0800

    add my files
    add tests

---
+++ packages/rdashk/src/index.ts
@@ -0,0 +1,8 @@
+import {getDiff} from "./fun";
+let date = new Date();
+console.log(date);
+let ans = getDiff(date);
+console.log(ans);
+let res = ans.split(".");
+let str_timer = `${res[0]} monts ${res[1]} days ${res[2]} hours`;
+console.log("Until the New Year left: ", str_timer);
