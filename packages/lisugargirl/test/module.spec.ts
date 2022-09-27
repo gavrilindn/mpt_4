5622034ce0e7f59d6242bdb6677a9a90adf5384b
lisugargirl <lisugargrl@gmail.com>
2022-09-27 14:45:24 +0800

    message

---
+++ packages/lisugargirl/test/module.spec.ts
@@ -0,0 +1,19 @@
+import { NYTime } from "../src/timeCounterToNY";
+import { expect } from "chai";
+
+describe("Example testing of module `module`", () => {
+
+  describe("function NYTime()", () => {
+    it("should return -1, when f(1) called", () => {
+      expect(NYTime(new Date(2022, 11, 17))).to.be.equal('До нового года осталось 15 дней 0 часов 0 минут 0 секунд ');
+    });
+
+    it("should return 0, when f(0) called", () => {
+      expect(NYTime(new Date(2022, 2, 22))).to.be.equal('До нового года осталось 285 дней 0 часов 0 минут 0 секунд ');
+    });
+
+    it("should return 0.379, when f(-0.379) called", () => {
+      expect(NYTime(new Date(2022, 4, 12))).to.be.equal('До нового года осталось 234 дней 0 часов 0 минут 0 секунд ');
+    });
+  });
+});
