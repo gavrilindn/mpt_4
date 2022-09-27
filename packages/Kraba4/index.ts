9b038b141fdec0026a4da5b29251b65a6730a36c
User <a.krasnov2001@mail.ru>
2022-09-27 13:35:26 +0800

    prettier rewrite

--- packages/Kraba4/index.ts
+++ packages/Kraba4/index.ts
@@ -1,4 +1,6 @@
-import {re} from './re'
-console.log(re(new Date(1000)))
-console.log(re(new Date(1000)) == "Осталось 364 дней 15 часов 59 минут 59 секунд 0 миллисекунд")
-
+import { re } from "./re";
+console.log(re(new Date(1000)));
+console.log(
+  re(new Date(1000)) ==
+    "Осталось 364 дней 15 часов 59 минут 59 секунд 0 миллисекунд"
+);
