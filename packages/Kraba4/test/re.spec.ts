a2b9304852f3743661e2ee3f196a45b7eb592de3
User <a.krasnov2001@mail.ru>
2022-09-27 13:28:00 +0800

    test add 2

--- packages/Kraba4/test/re.spec.ts
+++ packages/Kraba4/test/re.spec.ts
@@ -3,7 +3,7 @@ import { expect } from "chai";
 
 describe("time for various date", () => {
   it("date(1000)", () => {
-    expect(re(new Date(1000))).to.be.equal("Осталось 364 дней 15 часов 59 минут 59 секунд 0 миллисекунд");
+    expect(re(new Date(1000))).to.be.equal("Осталось 364 дней 23 часов 59 минут 59 секунд 0 миллисекунд");
   });
 
 });
