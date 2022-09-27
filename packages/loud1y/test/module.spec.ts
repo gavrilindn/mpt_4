e4203af8c63a64410e63b08f4cbbb71bf2b9af3d
loud1y <loud1y@mail.ru>
2022-09-27 13:45:35 +0800

    is one test enough?

---
+++ packages/loud1y/test/module.spec.ts
@@ -0,0 +1,13 @@
+// import { v1, f1 } from "../src/module";
+import { expect } from "chai";
+import {calculate} from "../src/script"
+
+describe("Example testing of module `module`", () => {
+
+  describe("function f1()", () => {
+    it("should return 365, when calculate(01.01.2022) called", () => {
+      expect(calculate(new Date(2022, 0, 1)).days).to.be.equal(365);
+    });
+  });
+  
+});
