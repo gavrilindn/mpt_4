dc549366c53001422933bce75407a89f5a8639d4
abc@abc.ru <rayman01@bk.ru>
2022-09-27 13:41:22 +0800

    pushing Not the repo

---
+++ packages/burcevdaniil/task2.ts
@@ -0,0 +1,14 @@
+export function calculateTimeDifference(date: Date){
+    var targetDate = new Date(date);
+    var today = new Date();
+    targetDate.setTime(targetDate.getTime() - targetDate.getTimezoneOffset()*60*1000);
+    today.setTime(today.getTime() - today.getTimezoneOffset()*60*1000);
+    var years = Math.abs(targetDate.getFullYear()-today.getFullYear());
+    var months = Math.abs(targetDate.getMonth() - today.getMonth());
+    var days = Math.abs(targetDate.getDay() - today.getDay());
+    var hours = Math.abs(targetDate.getHours() - today.getHours());
+    var minutes = Math.abs(targetDate.getMinutes() - today.getMinutes());
+    console.log(`${years} years, ${months} months, ${days} days, ${hours} hours and ${minutes} minutes`);
+}
+
+calculateTimeDifference(new Date());
