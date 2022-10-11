bedd57ed624fcbc0f86c835374effc537057d01d
my name <biba@lol.ru>
2022-10-11 12:41:59 +0800

    package VVan228

---
+++ packages/VVan228/tests/test.ts
@@ -0,0 +1,9 @@
+import { getTimeBeforeNY } from "../lib";
+import { expect } from 'chai';
+import { assert } from "console";
+
+describe('time to NY', () => { // the tests container
+    it('time from 1 Jan', () => { // the single test
+        expect(false).to.be.false;
+    });
+});
