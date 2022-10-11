95a0d291c528865ea33deb9c5e6d1c03612a11a0
my name <biba@lol.ru>
2022-10-11 13:18:57 +0800

    hardhat config

--- packages/VVan228/tests/test.ts
+++
@@ -1,9 +0,0 @@
-import { getTimeBeforeNY } from "../lib";
-import { expect } from 'chai';
-import { assert } from "console";
-
-describe('time to NY', () => { // the tests container
-    it('time from 1 Jan', () => { // the single test
-        expect(false).to.be.false;
-    });
-});
