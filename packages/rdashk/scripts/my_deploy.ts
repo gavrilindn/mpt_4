07a92d88dc75b3fdf384d818e05391458a734d9c
rdashk <rdashak8@gmail.com>
2022-10-18 14:01:01 +0800

    del smth

--- packages/rdashk/scripts/my_deploy.ts
+++ packages/rdashk/scripts/my_deploy.ts
@@ -1,12 +1,6 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
   const Zombie = await ethers.getContractFactory("ZombieFactory");
   const lock = await Zombie.deploy();
 
